8e312f98af5c17a6abea657a6e9a64b3bc5628df
Emman <emtsobrevega@gmail.com>
2025-10-07 01:46:49 +0800

    refactor: fix image filenames

--- src/app/vte/page.tsx
+++ src/app/vte/page.tsx
@@ -3,9 +3,7 @@ import { TestimonialsVte } from '@/components/sections/testimonials';
 import { ContactUs } from '@/components/sections/contact-us';
 import { AboutVteWCU } from '@/components/sections/about-wcu';
 import ResourcesSection from '@/components/sections/resource-section';
-import ServicesBento from '@/components/sections/services-bento';
 import AboutPreviewSection from '@/components/sections/about-company';
-import ServicesGrid from '@/components/sections-xtra/services-grid';
 import ServicesBento2 from '@/components/sections-xtra/services-bento2';
 
 export default function VelocityTaxLanding() {
